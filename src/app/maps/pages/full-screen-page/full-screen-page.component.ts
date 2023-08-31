819429675623d20be68685d9b2b7c691902b92a3
javtronic <juniorjavi7@gmail.com>
2023-08-30 21:32:34 -0500

    init seccion 21

---
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -0,0 +1,23 @@
+import { Component, AfterViewInit } from '@angular/core';
+
+import * as mapboxgl from 'mapbox-gl';
+
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoiamF2dHJvbmljIiwiYSI6ImNsbHloaDRuZjFreGYzaW85cTUzbXM1Z3EifQ.v--w0RJ4Eah7U0jGJboXsA';
+
+@Component({
+  templateUrl: './full-screen-page.component.html',
+  styleUrls: ['./full-screen-page.component.css']
+})
+export class FullScreenPageComponent implements AfterViewInit {
+  
+  
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9 // starting zoom
+  });
+  }
+
+}
